00a7bc47eff789d839dd7bc561b08dbe9a24c108
Vishalgiri8767 <vishalgiri8767@gmail.com>
2024-01-27 13:25:53 +0530

    hover effect apply on movie card

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,20 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { IMG_CDN_URL } from '../utils/constant'
 
 const MovieCard = ({posterPath, movieName, avgRating}) => {
-
+// const [movieInfo, setMovieInfo] = useState(null);
+  
+  const [isHovered, setIsHovered] = useState(false);
   if(!posterPath) return null;
 
+  const handleMouseEnter= () =>{
+    setIsHovered(true);
+    // setMovieInfo({movieName});
+  };
+  const handleMouseLeave =()=>{
+    setIsHovered(false);
+  };
+  const imageStyle = {
+    transform: isHovered ? 'scale(1.2)' : 'scale(1)', // Enlarge on hover
+    transition: 'transform 0.7s ease-in-out', // Smooth transition effect
+  };
 
   return (
-    <div className='w-44 h-60 pr-4 text-white'>
-      < img className='rounded-lg h-full w-full ' alt='movie name' 
+    <div className='w-56 h-80 pr-4 text-white flex justify-center items-center ' onMouseLeave={handleMouseLeave} onMouseEnter={handleMouseEnter}>
+      
+      < img className='rounded-lg h-64 w-48  ' alt='movie name' 
         src={IMG_CDN_URL + posterPath }
+        style={imageStyle}
       />  
+      {/* <div className='absolute top-80 '>
+        <p className=' pr-4 pt-0 bg text-lg '>{movieName}</p>
+        <p className='font-serif text-xs'>IMDB:   {avgRating}</p>
+      </div> */}
+
     </div>
 
-// <p className=' pr-4 pt-0 text-sm '>{movieName}</p>
-// <p className='font-serif text-xs'>IMDB:   {avgRating}</p>
   )
 }
 
